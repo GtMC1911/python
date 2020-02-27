f1db7934418c6908787639a0b663969ed62e9e12
riverriverstreeam <riverriverstreeam@gmail.com>
2020-02-27 18:33:41 +0900

    first

---
+++ twitter/schedule.py
@@ -0,0 +1,22 @@
+from time import sleep
+import schedule
+from requests_oauthlib import OAuth1Session
+CK = ""
+CS = ""
+AT = ""
+AS = ""
+update_url = "https://api.twitter.com/1.1/statuses/update.json"
+def tweet():
+  twitter = OAuth1Session(CK, CS, AT, AS)
+  body = "試験"
+  params = {"status" : body}
+  res = twitter.post(update_url, params = params)
+  if res.status_code == 200:
+    print("Success")
+  else:
+    print("code : %d"% res.status_code)
+if __name__ == "__main__":
+  schedule.every().day.at("15:40").do(tweet)
+  while True:
+    schedule.run_pending()
+    sleep(300)
